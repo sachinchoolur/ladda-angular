6bfdc24c663bdf00857ebff86abe4acf03b139df
Sachin <sachi77n@gmail.com>
2015-09-25 15:40:18 +0530

    Jscs fixes..

--- src/ladda-angular.js
+++ src/ladda-angular.js
@@ -1,23 +1,29 @@
-(function () {
+(function() {
     'use strict';
-    angular.module('ladda', []).directive('laddaButton', function () {
+    angular.module('ladda', []).directive('laddaButton', function() {
         return {
             restrict: 'A',
-            link: function (scope, element, attributes) {
+            link: function(scope, element, attributes) {
+
                 // Create ladda instance.
                 var l = Ladda.create(element[0]);
+
                 // Watch laddButton attribute;
-                scope.$watch(attributes.laddaButton, function (value) {
+                scope.$watch(attributes.laddaButton, function(value) {
+
                     // If directive value is number show progress bar
-                    if (typeof value === "number") {
+                    if (typeof value === 'number') {
                         if (!l.isLoading()) {
                             l.start();
                         }
+
                         l.setProgress(value / 100);
                     } else if (value === true) {
+
                         // If directive value is true start loading
                         l.start();
                     } else if (value === false) {
+
                         // If directive value is true start loading
                         l.stop();
                     }
